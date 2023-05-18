21ab1386ea5e42088bc60422fe2767b812491fdd
YaroslavPlaksii91 <yarosyk@ukr.net>
2023-05-19 01:34:28 +0300

    fix paths in main

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Gallery from '@/components/Gallery';
-import Advantages from '@/components/Advantages';
+import Hero from './Hero';
+import About from './About';
+import Gallery from './Gallery';
+import Advantages from './Advantages';
 import images from '../services/images.json';
 
 const Main: React.FC = () => {
