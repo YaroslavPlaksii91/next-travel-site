fcb0722e89777ce711b0782ff16b6d76947b3346
unknown <plaksii.yaroslav@gmail.com>
2023-05-22 14:36:20 +0300

    add lang attr to html

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n: {
+        locales: ["uk", "en"],
+        defaultLocale: "uk",
+      },
+}
 
 module.exports = nextConfig
